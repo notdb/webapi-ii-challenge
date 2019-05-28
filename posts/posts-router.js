0eb480e356635afc1a4029b1c6d685906ff6bc88
Doug <me@douglasbowen.com>
2019-05-28 16:14:36 -0400

    add get endpoint for /api/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,3 +1,19 @@
 const Posts = require("../data/db.js");
 
 const router = require("express").Router();
+
+//get requests
+
+router.get("/", async (req, res) => {
+  try {
+    const post = await Posts.find(req.query);
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "error retrieving the hubs"
+    });
+  }
+});
+
+module.exports = router;
